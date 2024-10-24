6f4bc31886b299f1d31eefe881a9affc5a32a20b
antaridj1 <antaridwijayanti@gmail.com>
2024-10-24 12:02:28 +0800

    feat: global css

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className="container p-3">
       <Navbar/>
       {/* <TodoList />
       <CompletedList /> */}
