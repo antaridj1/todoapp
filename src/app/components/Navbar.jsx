42df470e76900e185aaa9f527a64fed522ee586d
antaridj1 <antaridwijayanti@gmail.com>
2024-10-28 12:10:16 +0800

    fix: a href

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import BoxRounded from "./BoxRounded";
+import Link from "next/link";
 
 export default function Navbar() {
     const [darkMode, setDarkMode] = useState(false);
@@ -18,13 +19,13 @@ export default function Navbar() {
         <div className="fixed w-full p-3 top-0 z-50 start-0 animate-fade animate-once">
             <BoxRounded>
 				<div className="flex flex-wrap items-center justify-between py-6 px-2">
-                    <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                         <span className="nav-title self-center text-xl font-bold whitespace-nowrap dark:text-white">
                             <span className="text-black dark:text-white">Todo</span>
                             <span className="text-[#6C5FE5] dark:text-[#B9B1FF]">day</span>
                         </span>
-                    </a>
-                    <a onClick={() => setDarkMode(!darkMode)} className="cursor-pointer">
+                    </Link>
+                    <div onClick={() => setDarkMode(!darkMode)} className="cursor-pointer">
                         {darkMode ? (
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
@@ -34,7 +35,7 @@ export default function Navbar() {
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
                             </svg> 
                         )}
-                    </a>
+                    </div>
                     
                 </div> 
             </BoxRounded>
